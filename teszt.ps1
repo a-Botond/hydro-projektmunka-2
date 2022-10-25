cb9477d6879a8aa8784474ef48f798907da7f261
Soma Hamvai <115613500+Hamvai0709@users.noreply.github.com>
2022-10-25 13:29:32 +0200

    Update teszt.ps1
    update

--- teszt.ps1
+++ teszt.ps1
@@ -1,2 +1,36 @@
-﻿Write-Host "Hello World!"
-PowerShell -NoExit 
+function Teszt-Net {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [ValidateScript({
+          $Port = '53','80'
+          if ($_ -in $Port) { return $true } 
+          throw "'$_' is not a valid Port number. Please try one of the following: '$($port -join ',')'"
+        })]
+        [string]$Portno,
+        [Parameter()]
+        [ValidateNotNullOrEmpty()] 
+        [string]$Cmpname
+    )
+  
+    Test-NetConnection -Port $Portno -InformationLevel Detailed
+    Test-NetConnection -ComputerName $Cmpname -traceRoute -InformationLevel Detailed 
+   <#
+        .SYNOPSIS
+        Project work II
+
+        .DESCRIPTION
+        This is our second project at Óbuda University. 
+        This project is about: Automatic testing of client network settings. 
+        Data acquisition and analysis then creating understandable output for the users with Powershell.
+
+        .PARAMETER Name
+        Teszt-Net
+
+        .EXAMPLE
+        Teszt-Net [80] [www.google.com]
+
+        .LINK
+        Github: https://github.com/a-Botond/projektmunka-2
+    #>
+}
